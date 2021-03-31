20a73153d602e7d42054e240a78cf2c1463eebd0
Alessandro Silvestre <alessandro.silvestre.025@gmail.com>
2021-03-30 21:16:56 -0300

    Exercicio

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Salary;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Salary salary = new Salary();
+		
+		System.out.print("Digite o nome do funcionario: ");
+		salary.name = sc.nextLine();
+		System.out.print("Gross salary: ");
+		salary.grossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		salary.tax = sc.nextDouble();
+		
+		System.out.print("Employee: " + salary.name);
+		System.out.printf(", $ %.2f%n", salary.netSalary());
+		
+		System.out.print("Which percentage to increase salary? ");
+		double tax = sc.nextDouble();
+		salary.increaseSalary(tax);
+		
+		System.out.println();
+		System.out.println("Update data" + salary);
+		
+
+		sc.close();
+
+	}
+
+}
---
+++ src/entities/Salary.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Salary {
+	
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double netSalary() {
+		return grossSalary - tax;
+	}
+	
+	public void increaseSalary(double perc) {
+		this.grossSalary = grossSalary + ((perc / 100.0) * grossSalary);
+	}
+	
+	public String toString() {
+		return name 
+		+ ", $ "
+		+ String.format("%.2f", netSalary());
+	}
+
+}
